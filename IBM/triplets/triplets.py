e823225f11344510c0ebfb6a75efd53d0b604618
Brian-McHugh <125cedarstreet@gmail.com>
2021-02-02 22:08:30 -0600

    Add more efficient solution for triplets.py

--- IBM/triplets/triplets.py
+++ IBM/triplets/triplets.py
@@ -4,29 +4,48 @@ return the number of 3-int combinations that total up to the
 threshold or a sum less than the threshold.
 '''
 
-# O(n^3) time | O(n) space
-def triplets(nums, threshold):
+# O(n^2) time | O(n) space
+def triplets(nums, target):
     # if input list is not already sorted
-    #nums.sort()
-
-    # save our triplet solutions
-    triplets = []
+    nums.sort()
+    # number of 3-num combos that work 
+    combos = 0
     
-    for i in range(len(nums) - 2):
-        first = nums[i]
+    # since we need 3 nums we'll exclude last 2 nums for loop
+    for first_idx in range(len(nums) - 2):
+      mid_idx = first_idx + 1
+      last_idx = len(nums) - 1
+      while mid_idx < last_idx:
+        total = nums[first_idx] + nums[mid_idx] + nums[last_idx]
+        if total <= target:
+          combos += last_idx - mid_idx
+          mid_idx += 1
+        else:
+          last_idx -= 1
+    
+    return combos
 
-        for j in range(i + 1, len(nums) - 1):
-            middle = nums[j]
 
-            for k in range(j + 1, len(nums)):
-                last = nums[k]
-                current_sum = first + middle + last
+# O(n^3) time | O(n) space
+#def triplets(nums, target):
+    # save our triplet solutions
+    # triplets = []
 
-                if current_sum <= threshold:
-                    triplets.append([first, middle, last])
+    # for i in range(len(nums) - 2):
+    #     first = nums[i]
 
-    print(triplets)
-    return len(triplets)
+    #     for j in range(i + 1, len(nums) - 1):
+    #         middle = nums[j]
+
+    #         for k in range(j + 1, len(nums)):
+    #             last = nums[k]
+    #             current_sum = first + middle + last
+
+    #             if current_sum <= target:
+    #                 triplets.append([first, middle, last])
+
+    # print(triplets)
+    # return len(triplets)
 
 # Testing
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -36,3 +55,7 @@ print(triplets(nums, t)) # >>> 31
 nums_1 = [1, 2, 3, 4, 6]
 t_1 = 8
 print(triplets(nums_1, t_1)) # >>> 3
+
+nums_2 = [-2, 0, 1, 3]
+t_2 = 3
+print(triplets(nums_2, t_2)) # >>> 3
